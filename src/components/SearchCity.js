11e81d1a9f6d2c559a4a29714c9c8b1045c1c27a
Maiko Morales <maiko.moralesr@gmail.com>
2019-08-16 04:29:34 -0400

    Added units picker component

--- src/components/SearchCity.js
+++ src/components/SearchCity.js
@@ -1,24 +1,30 @@
-import React, { useState } from "react";
+import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
-import { Button, Form, FormControl, InputGroup } from 'react-bootstrap';
+import { Button, Form, FormControl, InputGroup, ToggleButton, ToggleButtonGroup } from 'react-bootstrap';
+
+const UNITS = {
+    FAHRENHEIT: 'imperial',
+    CELCIUS: 'metric',
+};
 
 const SearchCity = ({ onSubmit }) => {
-    const [ searchText, setSearchText ] = useState('');
+    const searchText = React.createRef();
+    const units = React.createRef();
 
     const submitForm = e => {
         e.preventDefault();
-        onSubmit(searchText);
-    };
-
-    const handleTextChange = e => {
-        setSearchText(e.target.value);
+        onSubmit(searchText.current.value, units.current.value);
     };
 
     return (
-        <Form onSubmit={submitForm}>
+        <Form onSubmit={submitForm} inline className="ml-auto">
+            <ToggleButtonGroup ref={units} type="radio" name="options" defaultValue={UNITS.FAHRENHEIT}>
+                <ToggleButton value={UNITS.FAHRENHEIT} variant="light">°F</ToggleButton>
+                <ToggleButton value={UNITS.CELCIUS} variant="light">°C</ToggleButton>
+            </ToggleButtonGroup>
             <InputGroup>
-                <FormControl autoFocus value={searchText} onChange={handleTextChange} placeholder="Search City..." />
+                <FormControl ref={searchText} autoFocus placeholder="Search City..." />
                 <InputGroup.Append>
                     <Button variant="outline-secondary" type="submit" >
                         <FontAwesomeIcon icon={faSearch} />
