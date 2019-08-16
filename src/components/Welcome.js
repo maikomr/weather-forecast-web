64d4178bb742f51cf960de31c37b27d70a05eb7e
Maiko Morales <maiko.moralesr@gmail.com>
2019-08-16 03:17:48 -0400

    Split paragraphs

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -5,9 +5,10 @@ const Welcome = () => (
     <Jumbotron>
         <h1>Welcome to Weather Forecast</h1>
         <p>
-            This is a simple weather forecast app that display High and Low temperature values for the next five days.
-            You can start typing the name of a city in the search bar above..
+            This is a simple weather forecast app that display 
+            High and Low temperature values for the next five days.
         </p>
+        <p>You can start typing the name of a city in the search bar above..</p>
     </Jumbotron>
 );
 
