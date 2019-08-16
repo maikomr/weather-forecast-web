ac0aebba366c9e412f997da06dba091df5b71c50
Maiko Morales <maiko.moralesr@gmail.com>
2019-08-16 03:04:27 -0400

    Made Welcome component a jumbotron

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Jumbotron } from 'react-bootstrap';
 
 const Welcome = () => (
-    <div>
+    <Jumbotron>
         <h1>Welcome to Weather Forecast</h1>
-        <h2>You can start by searching a city</h2>
-    </div>
+        <p>
+            This is a simple weather forecast app that display High and Low temperature values for the next five days.
+            You can start typing the name of a city in the search bar above..
+        </p>
+    </Jumbotron>
 );
 
 export default Welcome;
