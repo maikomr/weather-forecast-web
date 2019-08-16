683b47f7eecaf70eb62816367d3d0306c0cee9d1
Maiko Morales <maiko.moralesr@gmail.com>
2019-08-16 02:04:42 -0400

    Display only five days

--- src/reducers/weatherForecastReducer.js
+++ src/reducers/weatherForecastReducer.js
@@ -13,29 +13,40 @@ export const initialState = {
 
 const MID_DAY_TIME = '12:00:00';
 
+const takeFirstFiveElements = arr => arr.splice(0, 5);
+
+const convertObjectForecastToArray = dailyForecastObj => {
+    const dailyForecastArr = [];
+    for (const date in dailyForecastObj) {
+        dailyForecastArr.push({...dailyForecastObj[date], date })
+    }
+    return takeFirstFiveElements(dailyForecastArr);
+};
+
+const evaluateHighAndLowTemp = (currentDay, temp) => {
+    currentDay.lowTemp = Math.min(currentDay.lowTemp, temp);
+    currentDay.highTemp = Math.max(currentDay.highTemp, temp);
+};
+
+const evaluateOveralWeather = (time, currentDay, weather) => {
+    if (time === MID_DAY_TIME) {
+        currentDay.overallWeather = weather[0];
+    }
+};
+
 const getDailyForecast = records => {
     const dailyForecastObj = {};
     records.forEach(({ dt_txt, main: { temp }, weather }) => {
         const [ date, time ] = dt_txt.split(' ');
         if (dailyForecastObj.hasOwnProperty(date)) {
             const currentDay = dailyForecastObj[date];
-            currentDay.lowTemp = Math.min(currentDay.lowTemp, temp);
-            currentDay.highTemp = Math.max(currentDay.highTemp, temp);
-            if (time === MID_DAY_TIME) {
-                currentDay.overallWeather = weather[0];
-            }
+            evaluateHighAndLowTemp(currentDay, temp);
+            evaluateOveralWeather(time, currentDay, weather);
         } else {
-            dailyForecastObj[date] = {
-                lowTemp: temp,
-                highTemp: temp
-            };
+            dailyForecastObj[date] = { lowTemp: temp, highTemp: temp };
         }
     });
-    const dailyForecastArr = [];
-    for (const date in dailyForecastObj) {
-        dailyForecastArr.push({...dailyForecastObj[date], date })
-    }
-    return dailyForecastArr;
+    return convertObjectForecastToArray(dailyForecastObj);
 };
 
 const weatherForecastReducer = (state = initialState, action) => {
